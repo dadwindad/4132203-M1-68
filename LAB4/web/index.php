80082b798bf4c0af8ac8b2a69a11d13023730b46
dadwindad <vongpramate@gmail.com>
2025-12-09 12:06:57 +0700

    lab3+4

---
+++ LAB4/web/index.php
@@ -0,0 +1,22 @@
+<html>
+  <body>
+    <div id="hello">Hello PHP</div>
+
+    <div id="blog"></div>
+  </body>
+
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js"></script>
+
+  <script>
+    document.getElementById("hello").innerHTML = "Welcome";
+  
+    // $(function(){});
+
+    $(document).ready(function(){
+        $("#hello").html("I LOVE IT");
+
+        $("#blog").load("/page/blog.php");
+    });
+  
+  </script>
+</html>
